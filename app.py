eafbe25adf23b6f722341ee861e12e13416991a8
dwb217 <davidwbender@gmail.com>
2019-10-24 18:46:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,6 @@ app.layout = html.Div([
     html.Br(),
     dcc.Graph(id='display-value'),
     html.Br(),
-    dcc.Map(id='display-value'),
     html.A('Code on Github', href='https://github.com/austinlasseter/virginia_election_2016'),
     html.Br(),
     html.A('Data Source', href='https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/LYWX3D')
@@ -36,31 +35,6 @@ app.layout = html.Div([
 
 
 ######### Interactive callbacks go here #########
-@app.callback(dash.dependencies.Output('display-value', 'figure'),
-              [dash.dependencies.Input('dropdown', 'value')])
-def juris_picker(juris_name):
-    juris_df=df[df['jurisdiction']==juris_name]
-
-    mydata1 = go.Bar(x=list(juris_df['precinct'].value_counts().index),
-                     y=list(juris_df['votes']['Donald Trump']),
-                     marker=dict(color='#122A7F'),
-                     name='Trump')
-    mydata2 = go.Bar(x=list(juris_df['precinct'].value_counts().index),
-                     y=list(juris_df['votes']['Hillary Clinton']),
-                     marker=dict(color='#f96800'),
-                     name='Clinton')
-    mydata3 = go.Bar(x=list(juris_df['precinct'].value_counts().index),
-                     y=list(juris_df['votes']['Other']),
-                     marker=dict(color='#009900'),
-                     name='Other')
-
-    mylayout = go.Layout(
-        title='Votes by candidate for: {}'.format(juris_name),
-        xaxis=dict(title='Precincts'),
-        yaxis=dict(title='Number of Votes')
-    )
-    fig = go.Figure(data=[mydata1, mydata2, mydata3], layout=mylayout)
-    return fig
 
 
 @app.callback(dash.dependencies.Output('display-map', 'figure'),
